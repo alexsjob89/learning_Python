8273afc7b5ec367da8cc642ce265cc28e7f72fc4
alexsjob89 <103150434+alexsjob89@users.noreply.github.com>
2023-06-25 11:07:09 +0100

    test

--- main.py
+++ main.py
@@ -1,16 +1,32 @@
-# This is a sample Python script.
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+def calculator(a, b):
+    print(f"\nPerforming calculator on{a} and {b}")
+    print(f"{a} + {b} = {a + b}")
+    print(f"{a} + {b} = {a + b}")
+    print(f"{a} * {b} = {a * b}")
+    print(f"{a} / {b} = {a / b}")
+    print(f"{a} is greater than {b} is {a > b}")
+    print(f"{a} is equal to {b} is {a == b}")
+    print(f"{a} and {b} are the same object is {a is b}")
 
+def main():
+    print("This is a basic app to demonstrate the use of basic operators in Python.")
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+    a = int(input("\nEnter a number for 'a': "))
+    b = int(input("Enter a number for 'b' : "))
 
+    calculator(a, b)
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+    my_list = [a, b]
+    print(f"\nCreated list: {my_list}")
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    check_num = int(input("Enter a number to check if it is in the list: "))
+    print(f"{check_num} is in the list is {check_num in my_list}")
+
+    print("\nAssigning 'a' to 'c' (a = c)")
+    c = a
+    print(f"a: {a}, c: {c}")
+    print(f"'a' and 'c' point to the same object is {a is c}")
+
+ if __name__ == "__main__":
+        main()
